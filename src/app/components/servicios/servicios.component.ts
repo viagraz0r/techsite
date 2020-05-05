ed4daf710278d2d821687e60b48a70da0bc6e7d1
viagraz0r <waltherawad@gmail.com>
2020-05-04 22:49:58 -0500

    se añade logo en navbar

--- src/app/components/servicios/servicios.component.ts
+++ src/app/components/servicios/servicios.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ServiciosService } from 'src/app/services/servicios.service';
 
 @Component({
   selector: 'app-servicios',
   templateUrl: './servicios.component.html',
-  styleUrls: ['./servicios.component.css']
+  styleUrls: ['./servicios.component.css'],
 })
 export class ServiciosComponent implements OnInit {
+  constructor(private _serviciosService: ServiciosService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
